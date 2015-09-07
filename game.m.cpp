56c0884d37d2303ebb01b5902d1b9dcc7f3bed33
David <destessmargiassi@gmail.com>
2015-09-07 15:45:17 -0400

    update CMakeLists.txt and game.m.cpp

--- game.m.cpp
+++ game.m.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <iostream>
+#include <gtest/gtest.h>
 
-int main() 
+TEST( sample_test_case, sample_test )
 {
-	std::cout << "Hello World";
+	EXPECT_EQ( 1, 1 );
+}
+
+int main(int argc, char **argv) 
+{
+	std::cout << "Hello World"<<"\n";
+	::testing::InitGoogleTest (&argc, argv);
+	return RUN_ALL_TESTS();
 }
 
