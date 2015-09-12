8881a4cbaeac162ac7e48364e48a2adf239f4da8
David <destessmargiassi@gmail.com>
2015-09-11 23:19:43 -0400

    Remove extra iallocator.h/.cpp

--- src/engine/memory/iallocator.h
+++ src/engine/memory/iallocator.h
@@ -5,6 +5,16 @@ namespace StevensDev
 
 namespace sgdm
 {
-    
+
+template <class T>
+class iAllocator
+{
+  public:
+    //should it have constructor+copy+destructor?
+    virtual get( int count ) : T* =0;
+    virtual release( T*, int count) : void =0;
 }
-}
+
+
+} // End sdgm namespace
+} // End StevensDev namespace
